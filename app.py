6b4dc5c158e67dc4ce6dcadcccde7037eb9448a9
pazuzu71 <genchez.tula@gmail.com>
2024-01-13 19:04:40 +0300

    сохраняемся..

--- app.py
+++ app.py
@@ -7,8 +7,11 @@ from datetime import datetime
 
 import aioftp
 import aiosqlite
-from bs4 import BeautifulSoup
+import pytz
 
+
+host, port, login, password = 'ftp.zakupki.gov.ru', 21, 'free', 'free'
+links = []
 
 TABLE_FILES = '''create table if not exists files
     (
@@ -23,49 +26,80 @@ TABLE_FILES = '''create table if not exists files
     )'''
 
 
-async def insert_event(db, event_data):
+async def insert_event(db, row):
     """Insert a new event into the files table"""
     sql = """INSERT INTO files 
-              (ftppath, modify)  
-              VALUES (?, ?)"""
+              (ftppath, modify, eisdocno, eispublicationdate, xmlname, creationdate)  
+              VALUES (?, ?, ?, ?, ?, ?)"""
 
     await db.execute(sql, (
-       event_data['ftppath'],
-       event_data['modify']
+        row['ftp_path'],
+        row['modify'],
+        row['eisdocno'],
+        row['eispublicationdate'],
+        row['xmlname'],
+        datetime.now(pytz.timezone('Europe/Moscow')).isoformat(sep='T', timespec='auto')
     ))
     await db.commit()
 
 
-async def get_data(ftppath: str):
-    file = ftppath.split('/')[-1]
-    async with aioftp.Client.context(host, port, login, password) as client:
-        print(f"Downloading file {file}...")
-        await client.download(ftppath, f"Temp/{file}", write_into=True)
-        print(f"Finished downloading file {file} into Temp/{file}")
-        if file == 'contract_Tulskaja_obl_2024011200_2024011300_001.xml.zip':
-            z = zipfile.ZipFile(f'Temp//{file}', 'r')
-            for item in z.namelist():
-                if item.endswith('.xml'):
-                    print(f'Extract {item} from {ftppath}')
-                    z.extract(item, 'Temp')
+async def get_data(ftp_path: str, modify: str):
+    file = ftp_path.split('/')[-1]
+    # client = aioftp.Client()
+    # await client.connect(host=host)
+    # await client.login(user='free', password='free')
+    # print(f"Downloading file {file}...")
+    # await client.download(ftp_path, f"Temp/{file}", write_into=True)
+    # print(f"Finished downloading file {file} into Temp/{file}")
+    # await client.quit()
+    while True:
+        try:
+            async with aioftp.Client.context(host, port, login, password) as client:
+                print(f"Downloading file {file}...")
+                await client.download(ftp_path, f"Temp/{file}", write_into=True)
+                print(f"Finished downloading file {file} into Temp/{file}")
+            break
+        except ConnectionResetError:
+            print('ConnectionResetError')
+            pass
+    event_data = []
+    if file == 'contract_Tulskaja_obl_2024011200_2024011300_001.xml.zip':
+        z = zipfile.ZipFile(f'Temp//{file}', 'r')
+        for item in z.namelist():
+            if item.endswith('.xml'):
+                print(f'Extract {item} from {ftp_path}')
+                z.extract(item, 'Temp')
                 with open(f'Temp//{item}') as f:
                     src = f.read()
                     print(re.search(r'(?<=<regNum>)\d{19}(?=</regNum>)', src)[0],
                           re.search(r'(?<=<publishDate>).+(?=</publishDate>)', src)[0])
+                    event_data.append({
+                        'ftp_path': ftp_path,
+                        'modify': modify,
+                        'eisdocno': re.search(r'(?<=<regNum>)\d{19}(?=</regNum>)', src)[0],
+                        'eispublicationdate': re.search(r'(?<=<publishDate>).+(?=</publishDate>)', src)[0],
+                        'xmlname': item
+                    })
                 os.unlink(f'Temp//{item}')
-            z.close()
+        z.close()
+    async with aiosqlite.connect('sqlite.db') as db:
+        for row in event_data:
+            await insert_event(db, row)
 
 
 async def get_ftp_list():
     async with aioftp.Client.context(host, port, login, password) as client:
         x = await client.list('/fcs_regions/Tulskaja_obl/contracts/currMonth', recursive=False)
     for path, info in x:
-        # print(info)
+        print(path, info)
         if info['size'] != '22':
             # print(info)
-            res.append({
-                'ftppath': str(path),
-                'modify': datetime.strptime(info['modify'], '%Y%m%d%H%M%S').isoformat(sep='T', timespec='auto')})
+            links.append(
+                (str(path),
+                 datetime.strptime(info['modify'], '%Y%m%d%H%M%S').astimezone(pytz.timezone('Europe/Moscow')).isoformat(sep='T', timespec='auto'))
+            )
+
+            # links.append(str(path))
 
 
 async def main():
@@ -74,22 +108,19 @@ async def main():
     tasks = [
         asyncio.create_task(get_ftp_list())
     ]
-    await asyncio.wait(tasks)
-    # for p in res:
-    #     print
+    await asyncio.gather(*tasks)
     tasks = [
-        asyncio.create_task(get_data(dct['ftppath'])) for dct in res
+        asyncio.create_task(get_data(ftp_path, modify)) for ftp_path, modify in links
     ]
-    await asyncio.wait(tasks)
-
-    async with aiosqlite.connect('sqlite.db') as db:
-        for event_data in res:
-            await insert_event(db, event_data)
+    await asyncio.gather(*tasks)
+    # print(event_data)
+    # async with aiosqlite.connect('sqlite.db') as db:
+    #     for event_data in event_data:
+    #         await insert_event(db, event_data)
 
 
 if __name__ == '__main__':
-    host, port, login, password = 'ftp.zakupki.gov.ru', 21, 'free', 'free'
-    res = []
+
     if not os.path.exists('Temp'):
         os.mkdir('Temp')
     try:
