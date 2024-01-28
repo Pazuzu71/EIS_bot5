27ce064d72772b30edd69802e884cba7c0887822
pazuzu71 <genchez.tula@gmail.com>
2024-01-28 20:56:34 +0300

    Обрабатываем все типы кроме ПК. Убрал создание базы под постгресом, теперь база и пользователь создаются вручную.

--- app.py
+++ app.py
@@ -10,35 +10,26 @@ import aioftp
 import asyncpg
 from asyncpg.connection import Connection
 
-
+# TODO вынести все константы в отдельный файлы или в переменные окружения
 host, port, login, password = 'ftp.zakupki.gov.ru', 21, 'free', 'free'
 links = []
 folders = [
     # '/fcs_regions/Tulskaja_obl/contracts/currMonth',
     # '/fcs_regions/Tulskaja_obl/contracts/prevMonth',
     '/fcs_regions/Tulskaja_obl/notifications/currMonth',
-    # '/fcs_regions/Tulskaja_obl/notifications/prevMonth'
-
+    # '/fcs_regions/Tulskaja_obl/notifications/prevMonth',
+    # '/fcs_regions/Tulskaja_obl/plangraphs2020/currMonth',
+    # '/fcs_regions/Tulskaja_obl/plangraphs2020/prevMonth',
+    # '/fcs_regions/Tulskaja_obl/protocols/currMonth',
+    # '/fcs_regions/Tulskaja_obl/protocols/prevMonth',
 ]
+psql_connection_descriptor = dict(host='127.0.0.1', user='db_user', password='myPassword', database='psql_db')
 
-
-async def create_psql_db():
-    try:
-        conn = await asyncpg.connect(host='127.0.0.1', user='postgres', password='123', database='psql_db')
-        await conn.close()
-        print('База постгрес существует')
-    except asyncpg.InvalidCatalogNameError:
-        print('База постгрес не существует и будет создана')
-        conn = await asyncpg.connect(host='127.0.0.1', user='postgres', password='123', database='template1')
-        await conn.execute(
-            'CREATE DATABASE psql_db OWNER postgres'
-        )
-        await conn.close()
-        print('База постгрес создана')
+# TODO переделать подключения на пул подключений
 
 
 async def create_psql_tables():
-    conn: Connection = await asyncpg.connect(host='127.0.0.1', user='postgres', password='123', database='psql_db')
+    conn: Connection = await asyncpg.connect(**psql_connection_descriptor)
     await conn.execute('''
             create table if not exists zip
             (
@@ -61,8 +52,8 @@ async def create_psql_tables():
 
 
 async def insert_psql_zip(ftp_path: str, modify: str):
-
-    conn: Connection = await asyncpg.connect(host='127.0.0.1', user='postgres', password='123', database='psql_db')
+    """Добавление записи в таблицу zip"""
+    conn: Connection = await asyncpg.connect(**psql_connection_descriptor)
     await conn.execute('''INSERT INTO zip (ftp_path, modify, creationdate) VALUES ($1, $2, $3)''',
                        ftp_path, modify, datetime.now()
                        )
@@ -70,8 +61,9 @@ async def insert_psql_zip(ftp_path: str, modify: str):
 
 
 async def insert_psql_xml(row):
-
-    conn: Connection = await asyncpg.connect(host='127.0.0.1', user='postgres', password='123', database='psql_db')
+    """Добавление записи в таблицу xml"""
+    # TODO проверить конвертацию поля eispublicationdate в зонах времени
+    conn: Connection = await asyncpg.connect(**psql_connection_descriptor)
     zip_id = await conn.fetchval("""SELECT * FROM zip WHERE ftp_path = $1 AND modify = $2 AND enddate IS NULL""",
                                  row['ftp_path'], row['modify'], column=0)
     await conn.execute("""INSERT INTO xml (zip_id, eisdocno, eispublicationdate, xmlname) VALUES ($1, $2, $3, $4)""",
@@ -80,8 +72,7 @@ async def insert_psql_xml(row):
 
 
 async def exist_in_psql_db(ftp_path: str, modify: str):
-
-    conn: Connection = await asyncpg.connect(host='127.0.0.1', user='postgres', password='123', database='psql_db')
+    conn: Connection = await asyncpg.connect(**psql_connection_descriptor)
     is_exist = await conn.fetch("""SELECT * FROM zip WHERE ftp_path = $1 AND modify = $2 AND enddate IS NULL""",
                                 ftp_path, modify)
     await conn.close()
@@ -102,14 +93,13 @@ async def get_data(ftp_path: str, modify: str, semaphore):
                 print('ConnectionResetError')
                 pass
     event_data = []
-    # if file == 'contract_Tulskaja_obl_2024011200_2024011300_001.xml.zip':
+    # if file == 'notification_Tulskaja_obl_2024010100_2024010200_001.xml.zip':
     with zipfile.ZipFile(f'Temp//{file}', 'r') as z:
         for item in z.namelist():
-            # if item.endswith('.xml') and not any(
-            #         [item.startswith('contractAvailableForElAct'), item.startswith('contractProcedureCancel')]):
             if item.endswith('.xml') and any(
                     [item.startswith('contractProcedure_'), item.startswith('contract_'),
-                     all([item.startswith('epNotification'), not item.startswith('epNotificationCancel')])]):
+                     all([item.startswith('epNotification'), not item.startswith('epNotificationCancel')]),
+                     item.startswith('tenderPlan2020_'), item.startswith('epProtocol')]):
                 print(f'Extract {item} from {file}')
                 z.extract(item, 'Temp')
                 with open(f'Temp//{item}') as f:
@@ -120,10 +110,16 @@ async def get_data(ftp_path: str, modify: str, semaphore):
                             eispublicationdate = re.search(r'(?<=<publishDate>).+(?=</publishDate>)', src)[0]
                         except Exception as e:
                             print(e, item)
-                    if item.startswith('epNotification'):
+                    if any([item.startswith('epNotification'), item.startswith('epProtocol')]):
                         try:
                             eisdocno = re.search(r'(?<=<ns9:purchaseNumber>)\d{19}(?=</ns9:purchaseNumber>)', src)[0]
                             eispublicationdate = re.search(r'(?<=<ns9:publishDTInEIS>).+(?=</ns9:publishDTInEIS>)', src)[0]
+                        except Exception as e:
+                            print(e, item)
+                    if item.startswith('tenderPlan2020'):
+                        try:
+                            eisdocno = re.search(r'(?<=<ns5:planNumber>)\d{18}(?=</ns5:planNumber>)', src)[0]
+                            eispublicationdate = re.search(r'(?<=<ns5:publishDate>).+(?=</ns5:publishDate>)', src)[0]
                         except Exception as e:
                             print(e, item)
 
@@ -140,8 +136,8 @@ async def get_data(ftp_path: str, modify: str, semaphore):
 
                 os.unlink(f'Temp//{item}')
     os.unlink(f'Temp//{file}')
+    await insert_psql_zip(ftp_path, modify)
     if event_data:
-        await insert_psql_zip(ftp_path, modify)
         for row in event_data:
             await insert_psql_xml(row)
 
@@ -162,10 +158,9 @@ async def main():
     if not os.path.exists('Temp'):
         os.mkdir('Temp')
 
-    await create_psql_db()
     await create_psql_tables()
 
-    semaphore = asyncio.Semaphore(80)
+    semaphore = asyncio.Semaphore(50)
     tasks = [
         asyncio.create_task(get_ftp_list(folder, semaphore)) for folder in folders
     ]
